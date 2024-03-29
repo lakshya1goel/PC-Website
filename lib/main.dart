89efc3810d116bb7ab79f566ddc3e61ae594a6f4
lakshya1goel <lakshya1234goel@gmail.com>
2024-03-30 01:02:05 +0530

    update favicon & title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'PC Website',
+      title: 'Programming Club',
       routeInformationProvider: router.routeInformationProvider,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
